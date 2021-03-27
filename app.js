a98efa191e221849eea9bf4ca4c6fe02882ee695
Aldi-nafi43 <aldi.nafi43@gmail.com>
2021-03-27 11:19:24 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+alert('alert dari app.js')
+
+confirm('confirm dari app.js')
+
+console.log('console.log dari app.js')
+
+let a = 1
+let b = 9
+let c = a + b
+
+console.log(c)
+
+Date()
+let tanggal = Date()
+alert(tanggal)
+
+let nama = 'Nafi'
+document.getElementById('nama').innerHTML = nama
+
+
+document.getElementById('nama').style.color = 'Yellow'
+document.getElementById('nama').style.backgroundColor = 'Black'
+
+document.getElementById('nim').style.color = 'Red'
+
+
+function ubahWarna(){
+   document.getElementById('nim').style.backgroundColor = '#0b6623'
+}
+
